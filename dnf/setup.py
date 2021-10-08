b0e718defd4dd4d0fd341e97bb80f08e36078cd3
Kanitha Chim <chimkanitha@gmail.com>
2021-10-08 17:17:37 +0200

    Add installation breakpoint to setup.py

--- dnf/setup.py
+++ dnf/setup.py
@@ -5,18 +5,18 @@ with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 if sys.argv[1] not in ('sdist'):
-    raise RuntimeError("Install aborted - please install python-rpm from distributiong system.")
+    raise RuntimeError("Install aborted - please install python-dnf from distributiong system.")
 
 setup(
-    name='rpm',
-    version='0.0.3',
+    name='dnf',
+    version='0.0.2',
     url='https://github.com/JaneSoo/rpm-dnf/',
     license='GPLv2+',
-    author='RPM developers',
+    author='DNF developers',
+    author_email='ncoghlan@gmail.com',
     maintainer='Nick Coghlan',
     maintainer_email='ncoghlan@gmail.com',
-    home_page='https://github.com/JaneSoo/rpm-dnf/',
-    description='Dummy RPM package. Please use python-rpm provided by your OS.',
+    description='Dummy DNF package. Please use python-dnf provided by your OS.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     platforms='any',
